8018bd3c70b1f586c96bbf715edf15449a5c9a66
Adam Rosenblum <adamrosenblum05@gmail.com>
2024-12-17 17:46:52 -0800

    Deploy website to heroku.

---
+++ public/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
